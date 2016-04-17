c8cebbab4b0cd8b931696326e10146c8cb275fa3
Jalv <juanevz@gmail.com>
2016-04-17 11:22:39 +0200

    Messages conversation view  added

--- routes/messages.js
+++ routes/messages.js
@@ -6,6 +6,7 @@ module.exports = function (app) {
     getMessagesByUser = function(req,res) {
 
         var id_sender="";
+        var id_user="";
 
         console.log("getuserbymessage\n");
         console.log(req.params.username);
@@ -13,10 +14,18 @@ module.exports = function (app) {
         User.findOne({username: req.params.sender}, function (err, data) {
             console.log(data._id);
             id_sender = data._id;
-            console.log(id_sender);
-            Message.find({receiver: req.params.username,sender: id_sender},function(err,message){
+            User.findOne({username: req.params.username}, function (err, data2) {
+                console.log(data2._id);
+                id_user = data2._id;
+
+            console.log("IDS GET");
+            Message.find({$or:[{receiver: req.params.username,sender: id_sender},{receiver: req.params.sender,sender: id_user}]},function(err,message){
                 console.log(message);
-                res.status(200).send(message);
+                User.populate(message,{path:"sender"},function(err,m){
+                    res.status(200).send(message);
+                });
+
+            });
             });
         });
 
@@ -115,7 +124,7 @@ module.exports = function (app) {
     app.get('/allmessages', allMessages);
     app.get('/messages/:username/:sender',getMessagesByUser);
     app.post('/addmessage', addMessage);
-    app.get('/messages/', findMessages);
+    app.get('/messages/:username', findMessages);
     app.delete('/message/:id', deleteMessage);
 
 
