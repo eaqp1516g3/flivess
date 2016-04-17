9d9f55f3adbaa7a0ec19a19c70309b92019a3dfc
Aitor Piñeyro <itor95@gmail.com>
2016-04-17 17:33:14 +0200

    With the new profile and messages templates

--- public/controllers/homeCtl.js
+++ public/controllers/homeCtl.js
@@ -3,10 +3,5 @@
  */
 
 angular.module('Flivess').controller('homeCtl', ['$scope', '$http', function($scope, $http) {
-    console.log($scope.myVar);
-    $scope.myVar = !$scope.myVar;
-    console.log($scope.myVar);
-    $scope.message = "Hello from Home Controller";
-
 
 }]);
--- public/controllers/loginCtl.js
+++ public/controllers/loginCtl.js
@@ -4,7 +4,10 @@
 
 angular.module('Flivess').controller('loginCtl', ['$scope', '$http', '$cookies', function($scope, $http, $cookies)  {
     console.log("DENTRO DE loginCtl");
+
     $scope.login = function () {
+        console.log($scope.user);
+        console.log($scope.user.password);
         $http.post('/login', $scope.user).success(function (response) {
             console.log(response);
             $cookies.putObject('user',response);
