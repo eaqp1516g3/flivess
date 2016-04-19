8ebc879925a07d225ac1dfd666048bb1dcdc8f5a
sergiogm4 <sgonzalezmontilla4@gmail.com>
2016-04-19 19:55:48 +0200

    Register added frontend

--- public/controllers/register.js
+++ public/controllers/register.js
@@ -3,14 +3,60 @@
  */
 var base_url_prod="http://localhost:3000"
 
-angular.module('Flivess').controller('registerCtl', ['$scope', '$http', '$cookies', function($scope, $http, $cookies) {
+angular.module('Flivess').controller('registerCtl', ['$scope', '$http', '$cookies','$window', function($scope, $http, $cookies,$window) {
 
     $scope.registerUser = function(){
         console.log("USUARIO: " + $scope.user);
-        $http.post(base_url_prod+'/user', $scope.user).success(function (response){
-            $cookies.putObject('user',response);
-            window.location.href = "index.html";
+        if($scope.user.password == $scope.reenterpass && $scope.user.username!='' && $scope.user.email!='' && $scope.user.password!=''){
+            $http.post(base_url_prod+'/user', $scope.user).success(function (response){
+                $cookies.putObject('user',response);
+                $scope.alert.message="";
+                window.location.href = "index.html";
 
-        })
+
+            })
+        }
+        if($scope.user.username=='' || $scope.user.email=='' || $scope.user.password==''){
+            $scope.alertReg=true;
+            $scope.alert.message="The fields with * are mandatory";
+
+        }
+
+        else{
+            $scope.alertReg=true;
+            $scope.alert.message="Passwords  dont match";
+            //$window.alert("Passwords don't match");
+        }
     };
+
+
+    $scope.closeAlert=function(){
+        $scope.alertReg=false;
+        $scope.alert.message="";
+    }
+
+
+
+
 }]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
