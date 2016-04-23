a73822e6bdda4a64a74f219a1c17ee580131691d
sergiogm4 <sgonzalezmontilla4@gmail.com>
2016-04-23 13:01:57 +0200

    Register corrections and search user added

--- public/controllers/indexCtl.js
+++ public/controllers/indexCtl.js
@@ -14,4 +14,36 @@ angular.module('Flivess').controller('indexCtl', ['$scope', '$http',  function($
         console.log("Obtengo users");
     });
 
+
+
+    var _selected;
+
+    $scope.selected = undefined;
+    //$scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+    // Any function returning a promise object can be used to load values asynchronously
+
+
+    $scope.ngModelOptionsSelected = function(value) {
+        if (arguments.length) {
+            _selected = value.username;
+        } else {
+            return _selected;
+
+        }
+    };
+
+    $scope.modelOptions = {
+        debounce: {
+            default: 500,
+            blur: 250
+        },
+        getterSetter: true
+    };
+
+
+
+
+
+
+
 }]);
