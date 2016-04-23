3e082d2febc0b90a10bb94df3abcf53f19cfc5c2
sergiogm4 <sgonzalezmontilla4@gmail.com>
2016-04-23 15:34:19 +0200

    Provisional navbar changes (must be fixed)

--- public/controllers/indexCtl.js
+++ public/controllers/indexCtl.js
@@ -4,24 +4,42 @@
 
 
 var base_url_prod="http://localhost:3000"
-angular.module('Flivess').controller('indexCtl', ['$scope', '$http',  function($scope, $http) {
+angular.module('Flivess').controller('indexCtl', ['$scope', '$http', '$cookies',  function($scope, $http,$cookies) {
 
 
-    $scope.message="Hello";
+    //Nav controller
+    $scope.checkNavBar = function() {
+        var isLogged = $cookies.getObject('user');
+        console.log('Este es el logeado: ' + isLogged);
+        if (!angular.isUndefined(isLogged)) {
+            $scope.navLogged = true;
+            $scope.navInit = false;
+            console.log('1');
+
+        }
+        else{
+            $scope.navInit = true;
+            $scope.navLogged = false;
+            console.log('2');
+        }
+
+    }
+
+    $scope.checkNavBar();
+
+    $scope.logOut = function() {
+        $cookies.remove('user');
+    };
+//Search functions
 
     $http.get(base_url_prod+'/allusers/').success(function(data) {
         $scope.users = data;
         console.log("Obtengo users");
     });
 
-
-
     var _selected;
 
     $scope.selected = undefined;
-    //$scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-    // Any function returning a promise object can be used to load values asynchronously
-
 
     $scope.ngModelOptionsSelected = function(value) {
         if (arguments.length) {
@@ -39,6 +57,7 @@ angular.module('Flivess').controller('indexCtl', ['$scope', '$http',  function($
         },
         getterSetter: true
     };
+//--------------------------------------
 
 
 
