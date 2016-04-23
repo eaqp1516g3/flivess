bd7d6aaa99191075b24c26411bbb0858f117311a
marcelusDSA <marcelusz@hotmail.com>
2016-04-23 16:45:46 +0200

    resolved conflict

--- public/controllers/registerCtl.js
+++ public/controllers/registerCtl.js
@@ -13,28 +13,17 @@ angular.module('Flivess').controller('registerCtl', ['$scope', '$http', '$cookie
 
         console.log("USUARIO: " + $scope.user);
         if($scope.user.password == $scope.reenterpass && !angular.isUndefined($scope.user.username) && !angular.isUndefined($scope.user.email) && !angular.isUndefined($scope.user.password)){
-            $http.post(base_url_prod+'/user', $scope.user).then(function successCallback(response){
+            $http.post(base_url_prod+'/user', $scope.user).success(function(response){
                 console.log($scope.user.username);
                 $cookies.putObject('user',response);
                 $scope.alert.message="";
                 window.location.href = "#/home";
-
-
-<<<<<<< HEAD
             }).error(function (response) {
-
-
                      $scope.alertReg = true;
                      $scope.alert.message="Username already exists";
        
                 });
-=======
-            }, function errorCallback(response){
 
-                //IF ERROR, SHOW 'USERNAME ALREADY EXISTS'
-
-            })
->>>>>>> 3e082d2febc0b90a10bb94df3abcf53f19cfc5c2
         }
        if(angular.isUndefined($scope.user.username)||angular.isUndefined($scope.user.email) || angular.isUndefined($scope.user.password)){
             $scope.alertReg = true;
