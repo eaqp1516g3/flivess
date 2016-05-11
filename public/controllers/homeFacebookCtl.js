3742b684abcd8334a751aebd2e27b3f4b3893a26
Aitor Piñeyro <itor95@gmail.com>
2016-05-11 20:35:41 +0200

    Update activity and following, creat historical

--- public/controllers/homeFacebookCtl.js
+++ public/controllers/homeFacebookCtl.js
@@ -10,11 +10,13 @@ angular.module('Flivess').controller('homeFacebookCtl', ['$scope', '$http', '$co
 
     var loadUser = function(){
         $http.get(base_url_prod + '/users/user/facebook/' + $routeParams.facebook_id).success(function (response) {
+            console.log("QUE ME DEVUELVES PAYO?");
             console.log(response[0]);
             $cookies.putObject('user',response[0]);
             $rootScope.userlog = $cookies.getObject('user');
+            $location.path('/home');
         });
-        $location.path('/home');
+
     }
     loadUser();
 }]);
