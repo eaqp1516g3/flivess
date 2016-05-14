abf669eda9cdf4275a2db2f33522f7259023d7ae
Aitor Piñeyro <itor95@gmail.com>
2016-05-15 00:25:25 +0200

    Put the static map in home, tracks in profiles and login and register in the same html/controller

--- public/controllers/profileCtl.js
+++ public/controllers/profileCtl.js
@@ -130,10 +130,34 @@ angular.module('Flivess').controller('profileCtl', ['$scope', '$http', '$cookies
     };
 
 
+    $http.get(base_url_prod + '/tracks/' + $routeParams.friend).success(function (data) {
+        console.log(data);
+        if(data=="") {
+            $scope.noFollowing = true;
+        }
+        else{
+            var int=0;
+            for (var i = 0; i < data.length; i++) {
+                $http.get(data[i].pointsurl).success(function (datos) {
+                    for (var i = 0; i < datos.length; i++) {
+                        if (i == 0) data[int].path = datos[i].latitude + "," + datos[i].longitude;
+                        else data[int].path += "|" + datos[i].latitude + "," + datos[i].longitude;
+                    }
+                    data[int].center = datos[1].latitude + "," + datos[1].longitude;
+                    var marker1 = datos[0].latitude + "," + datos[0].longitude;
+                    var marker2 = datos[datos.length -1].latitude + "," + datos[datos.length -1].longitude;
 
-
-
-
-
-
+                    //strokeColor: "#FF0000",
+                    //strokeOpacity: 0.7,
+                    //strokeWeight: 3
+                    data[int].img="http://maps.googleapis.com/maps/api/staticmap?center=" + data[int].center+"&zoom=17&size=425x180&maptype=roadmap&path=color:0xff0000ff|weight:3|"+data[int].path +"&sensor=false&markers=color:blue%7Clabel:S%7C"+marker1+"&markers=color:red%7Clabel:F%7C"+marker2;
+                    //zoom:17
+                    //mapTypeId: google.maps.MapTypeId.ROADMAP
+                    int++;
+                });
+            }
+            console.log(data);
+            $scope.routes = data;
+        }
+    });
 }]);
