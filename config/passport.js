a4f88fe60ef5ecdc494e3a45da5fab6e993c97a0
Aitor Piñeyro <itor95@gmail.com>
2016-05-17 11:34:51 +0200

    Some updates

--- config/passport.js
+++ config/passport.js
@@ -55,7 +55,7 @@ module.exports = function(passport) {
                             newUser.username = profile._json.name;
                             newUser.fullname = profile._json.name;
                             newUser.email = profile.emails[0].value;
-                            newUser.imgurl = profile.photos[0].value;
+                            newUser.imgurl = 'http://graph.facebook.com/'+ profile._json.id +'/picture?width=270&height=270';
 
                             newUser.save(function (err) {
                                 if (err)
